96e3d49efe1f37c95d646524b344add7757a8dc6
Jose Sanchez <josemsanchezq00@gmail.com>
2023-11-22 19:54:42 -0700

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:josemsanchezq00@gmail.com">josemsanchezq00@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/jose-manuel-sanchez-quintana">linkedin.com/JoseManuelSanchezQuintana</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Jose-Manuel-Sanchez-Quintana">github.com/Jose-Manuel-Sanchez-Quintana</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
